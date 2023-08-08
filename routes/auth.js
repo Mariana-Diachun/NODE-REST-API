23f54c4d4a9748bec571c98c3d317377c2871664
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-08-08 18:47:16 +0100

    add router post-register new user, add bcrypt and hashed password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("hey its auth route");
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //   generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashadPassword = await bcrypt.hash(req.body.password, salt);
+    // create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashadPassword,
+    });
+    //   save user and return response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
