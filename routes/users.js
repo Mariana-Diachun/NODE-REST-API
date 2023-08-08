56a21b0f366bdbb7d2fe9067615696563055225b
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-08-08 19:14:07 +0100

    update User schema

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,12 @@
+const User = require("../models/User");
 const router = require("express").Router();
+
+
+// update user
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 router.get("/", (req, res) => {
   res.send("hey its user route");
