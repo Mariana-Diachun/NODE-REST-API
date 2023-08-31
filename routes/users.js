185f7e0480f201c5a408b64fe32953490f6bde0f
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-08-31 20:59:56 +0100

    completed users requests

--- routes/users.js
+++ routes/users.js
@@ -59,7 +59,7 @@ router.put("/:id/follow", async (req, res) => {
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({
-          $push: { followings: req.body.userId },
+          $push: { followings: req.params.id },
         });
         res.status(200).json("user has been followed");
       } else {
@@ -69,9 +69,29 @@ router.put("/:id/follow", async (req, res) => {
       res.status(500).json(error);
     }
   } else {
-    res.status(403).json("you can follow yourself");
+    res.status(403).json("you can not follow yourself");
   }
 });
 // unfollow a user
-
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({
+          $pull: { followings: req.params.id },
+        });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you dont follow this user");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("you can not unfollow yourself");
+  }
+});
 module.exports = router;
